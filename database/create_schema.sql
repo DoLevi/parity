34a12d240f0c60cabf265870f9af6c5e3b5c48d1
DoLevi <domi.genz@gmail.com>
2020-07-17 17:13:11 +0200

    Initial commit

---
+++ database/create_schema.sql
@@ -0,0 +1,37 @@
+CREATE SCHEMA IF NOT EXISTS parity_dev;
+
+CREATE TABLE parity_dev.parity_games (
+	id UUID DEFAULT uuid_generate_v4(),
+	name VARCHAR(64) NOT NULL,
+	description VARCHAR(1024) NULL,
+
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE parity_dev.positions (
+	id UUID DEFAULT uuid_generate_v4(),
+	name VARCHAR(64) NOT NULL,
+	fk_parity_game UUID NOT NULL,
+
+	initial BOOLEAN NOT NULL,
+	player_0 BOOLEAN NOT NULL,
+	parity INTEGER NOT NULL,
+
+	PRIMARY KEY(id),
+	FOREIGN KEY(fk_parity_game) REFERENCES parity_games(id),
+	UNIQUE(name, fk_parity_game)
+);
+
+CREATE TABLE parity_dev.edges (
+	id UUID DEFAULT uuid_generate_v4(),
+	name VARCHAR(64) NOT NULL,
+	fk_parity_game UUID NOT NULL,
+	fk_source UUID NOT NULL,
+	fk_target UUID NOT NULL,
+
+	PRIMARY KEY(id),
+	FOREIGN KEY(fk_parity_game) REFERENCES parity_games(id),
+	FOREIGN KEY(fk_source) REFERENCES positions(id),
+	FOREIGN KEY(fk_target) REFERENCES positions(id),
+	UNIQUE(name, fk_source, fk_target)
+);
