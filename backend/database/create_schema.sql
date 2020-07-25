9dabdfc7133b125475558791f810f469e3153179
DoLevi <domi.genz@gmail.com>
2020-07-25 23:47:28 +0200

    fixed sql syntax error

--- backend/database/create_schema.sql
+++ backend/database/create_schema.sql
@@ -18,8 +18,8 @@ CREATE TABLE positions (
 	player_0 BOOLEAN NOT NULL,
 	parity INTEGER NOT NULL,
 
-	position_x: INTEGER NOT NULL,
-	position_y: INTEGER NOT NULL,
+	position_x INTEGER NOT NULL,
+	position_y INTEGER NOT NULL,
 
 	PRIMARY KEY(id),
 	FOREIGN KEY(fk_parity_game) REFERENCES parity_games(id),
